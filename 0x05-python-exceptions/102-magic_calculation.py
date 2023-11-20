ed28ec7d391856e534cf6d786168a8f5003db982
aaustinoduor <aaustinoduorr@gmail.com>
2023-11-20 10:39:32 -0800

    Python - Exceptions

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for r in range(1, 3):
+        try:
+            if r > a:
+                raise Exception("Too far")
+            result += (a ** b) / r
+        except Exception:
+            result = b + a
+            break
+    return result
