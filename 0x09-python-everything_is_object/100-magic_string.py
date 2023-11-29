40968f5a366b0fde3d00f9580127ef9c19af599b
aaustinoduor <aaustinoduorr@gmail.com>
2023-11-29 10:13:36 +0300

    Updated

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+""" Magic String"""
+
 
 def magic_string(my_list=[]) -> str:
+    """_summary_
+
+    Args:
+        my_list (list, optional): _description_. Defaults to [].
+
+    Returns:
+        str: _description_
+    """
     my_list.append("BestSchool")
     return ', '.join(my_list)
