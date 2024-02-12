e635bbccf41dde1cf32dfec392b791c64c5db204
aaustinoduor <aaustinoduorr@gmail.com>
2024-02-12 22:56:36 +0300

    avaScript - Warm up

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function factorial (r) {
+  if (r < 0) {
+    return (-1);
+  }
+  if (r === 0 || isNaN(r)) {
+    return (1);
+  }
+  return (r * factorial(n - 1));
+}
+
+console.log(factorial(Number(process.argv[2])));
