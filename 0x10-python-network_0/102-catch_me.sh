ad351c55e12494a2acecdd0ab951ea280820c71b
aaustinoduor <aaustinoduorr@gmail.com>
2024-03-01 01:11:36 +0300

    Updated task 6

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Catch me
-curl -sLX GET -d user_id=98 -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+curl -sLX POST -d user_id=98 -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
