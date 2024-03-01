29fb6cad182bc7062cbcaef048ebf044a5d93183
aaustinoduor <aaustinoduorr@gmail.com>
2024-03-01 15:55:16 +0300

    Updated task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """What's my status? #1
 """
-
 import requests
+
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     response = requests.get(url)
-    
+
     print("Body response:")
     print("\t- type:", type(response.text))
     print(f"\t- content: {response.text}")
