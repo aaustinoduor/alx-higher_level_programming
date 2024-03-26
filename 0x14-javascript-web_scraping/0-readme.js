e10e533e04049a0e560491d3bd133866526a1b04
aaustinoduor <aaustinoduorr@gmail.com>
2024-03-26 22:40:09 +0300

    web_scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,18 +1,7 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 // Import the built-in Node.js 'fs' module.
 
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  // use fs.readFile() to read contents of file specified as a command-line argument
-  // 'utf8' specifies the encoding of the file being read
-
-  if (error) {
-    // if an error occurs during file read operation, 'error' parameter will contain an error object.
-    console.error('Error reading the file:', error);
-
-  } else {
-    // if file is read successfully, 'content' parameter will contain contents of file as a string.
-    console.log(content);
-  }
+  console.log(error || content);
 });
