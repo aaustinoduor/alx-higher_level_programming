7c27936287a9cda4d9523bb622cf004da1a54bf2
aaustinoduor <aaustinoduorr@gmail.com>
2024-03-26 20:39:30 +0300

    web_scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,45 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, body, response) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
+
+const movieId = process.argv[2];
+
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+//  the 'request' module to perform HTTP GET request to Star Wars API URL.
+request(apiUrl, function (error, response, body) {
+  // check if there was no error during HTTP request
+  if (!error && response.statusCode === 200) {
+    // parse JSON response bod
+    const movieData = JSON.parse(body);
+    // Create an array of promises that fetch data for each individual character.
+    const characterPromises = movieData.characters.map((characterUrl) => {
+      return new Promise((resolve, reject) => {
+        // use another 'request' to fetch data for individual character.
+        request(characterUrl, function (charError, charResponse, charBody) {
+          // check if there was no error during HTTP request
+          if (!charError && charResponse.statusCode === 200) {
+            // parse JSON response body
+            const characterData = JSON.parse(charBody);
+            // resolve promise with name of character.
+            resolve(characterData.name);
+          } else {
+            // reject promise with error message if  there was an error during HTTP request
+            reject(new Error(`Error fetching character data: ${charError}`));
+          }
+        });
+      });
+    });
+
+    Promise.all(characterPromises)
+      .then((characterNames) => {
+        console.log(characterNames.join('\n'));
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  } else {
+    console.error('Error fetching movie data:', error);
   }
 });
-
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
-    }
-  });
 }
